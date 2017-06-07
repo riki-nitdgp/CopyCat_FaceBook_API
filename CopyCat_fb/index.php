a7cfa0416d7ee8597730c3c0b1d16d145629a382
riki-nitdgp <rkmondal41@gmail.com>
2017-06-06 22:26:07 -0700

    Update index.php
    Add Bootstrap for Better UI

--- CopyCat_fb/index.php
+++ CopyCat_fb/index.php
@@ -1,13 +1,51 @@
-<html>
-<script></script>
 
-<body>
-<h1> Write Your Favriot Quote</h1>
-<form name="" method="post" action="post.php">
-<input type="textarea" name="message">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Facebook Quote</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script>
 
-<input type="submit" value="Post On FB">
-</form>
+  </script>
+</head>
+
+<body background='image/1.jpg'>
+  <div class="container-fluid">
+  	<div class="row">
+  			<div class="col-lg-12">
+  					<h1 class="page-header">
+  							<h1> Facebook API to Fetche your Favorite Quote And Post It As Image</h1>
+  					</h1>
+  			</div>
+  	</div>
+
+
+    <div class="row">
+     		 <div class="container">
+           <div class="panel panel-danger">
+             <div class="panel-heading">Write Your Any Favourite Quote</div>
+             <div class="panel-body">
+               <div class="col-lg-6 col-md-12">
+                 <form name="" method="post" action="post.php">
+                 <div class="form-group">
+                   <label for="quote"></label>
+                   <textarea type="text" name="message"class="form-control" id="quote" placeholder="Write Your Any Favourite Quote" required=""></textarea>
+
+                 </div>
+                 <button type="submit" class="btn btn-primary">Post In Facebook</button>
+
+               </form>
+             </div>
+             </div>
+           </div>
+
+            </div>
+          </div>
+</div>
 <body>
 
 </html>
